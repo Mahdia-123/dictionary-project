326eda6da7cd2cf734d6e82a670f26d8110164a3
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-07-23 09:49:07 +0430

    added the apiurl

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,9 +3,16 @@ import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState(null);
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyWord} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
+    axios.get(apiUrl).then(handleResponse);
+    console.log(apiUrl);
   }
   function handleKeywordChange(event) {
     setKeyWord(event.target.value);
